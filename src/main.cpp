8f12364a845f817568bd33a35143c41dbe1f04b3
AlAminAshik <106368872+AlAminAshik@users.noreply.github.com>
2025-01-29 15:31:53 +0600

    rfid code and lib added

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,30 @@
-#define ledPin 13
+// The Arduino framework is used to interact with the hardware rfid 6300 module.
 
 #include <Arduino.h>
+#include <rdm6300.h>
+#define RDM6300_RX_PIN 2                        //rfid module to arduino pin 2(Rx)
+Rdm6300 rdm6300;
 
+int value = 0;
 
 void setup(){
-  pinMode(ledPin, OUTPUT);
   Serial.begin(9600);
-  Serial.println("Hello World");
+  rdm6300.begin(RDM6300_RX_PIN);                     //serial start for module
+  Serial.println("\nPlace RFID tag near the rdm6300...");  
+}
+
+//Reading the RFID card
+void readTags() {
+  if (rdm6300.get_new_tag_id()){                   //rfid card availability check
+  delay(10);                                       //wait for read card
+  value = rdm6300.get_tag_id();                    //get rfid tag
+  Serial.println(value);                           // print rfid tag
+  }                               
+  rdm6300.end();                                   //IMPORTANT- removes multiple read
+  rdm6300.begin(RDM6300_RX_PIN);                   //IMPORTANT- removes multiple read
 }
 
 void loop() {
-  digitalWrite(ledPin, HIGH);
-  delay(1000);
-  digitalWrite(ledPin, LOW);
-  delay(1000);
-  Serial.println("Hello World");
+ readTags();                                     //check tag repeatedly
+ delay(100);
 }
